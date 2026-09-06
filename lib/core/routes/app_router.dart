b9d14de9a3f17a6d229ea94df91859c8db3c6fd8
Belal Mohamed <dixen.bugs@gmail.com>
2026-09-06 12:54:52 +0300

    remove onboarding featuer

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:snap_chat/features/onboarding/presentation/pages/onboarding_page.dart';
 import 'package:snap_chat/features/splash/presentation/splash_page.dart';
 
 class AppRouter {
@@ -11,12 +10,6 @@ class AppRouter {
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return const SplashPage();
-        },
-      ),
-      GoRoute(
-        path: '/onboarding',
-        builder: (BuildContext context, GoRouterState state) {
-          return const OnboardingPage();
         },
       ),
     ],
